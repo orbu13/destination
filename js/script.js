0b96e3726b0ea7c6632393de90c4a82b7b031379
Student <hailu.elle@gmail.com>
2023-06-12 13:22:36 -0700

    update js file

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
-function PlacesIBeen(){
-    this.destinations = {};
+function PlacesIBeen() {
+    this.places = {};
     this.currentId = 0;
 }
-PlacesIBeen.prototype.addDestination = function(destination){
-    destination.id = this.assignId();
+PlacesIBeen.prototype.addPlace = function(place) {
+    place.id = this.assignId();
+    this.places[place.id] = place;
+};
+PlacesIBeen.prototype.assignId = function() {
+    this.currentId +=1;
+    return this.currentId;
+};
 
-this.destinations[destination.id] = destination;
+PlacesIBeen.prototype.findPlace = function(id) {
+    if(this.places[id] !== undefined) {
+        return this.places[id];
+    }
+    return false;
+}
+
+PlacesIBeen.prototype.deletePlace = function(id) {
+    if(this.places[id] === undefined) {
+        return false;
+    }
+    delete this.places[id];
+    return true;
 };
-PlacesIBeen.prototype.assignId = function (){
-    this.currentId +=1
-    return this.currentId
-};
-function D(location, landmarks, timeOfYear, notes){
+
+function Place(location, landmarks, timeOfYear, notes) {
     this.location = location
     this.landmarks = landmarks
     this.timeOfYear = timeOfYear
     this.notes = notes
 }
-
-let destination1 = new D("portland", "bridges", "june", "rain");
-console.log(destination1);
+// let destination1 = new Places("portland", "bridges", "june", "rain");
+// console.log(destination1);
